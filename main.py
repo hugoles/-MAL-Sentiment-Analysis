0a67995ab2bb9f99463ca6cb631d304c0e4f4a4c
Hugo Dutra <67278688+hugoles@users.noreply.github.com>
2024-01-04 17:55:28 -0300

    update-1
    feat: Start implementation of code for extracting data from MyAnimeList
    
    In this commit, the initial code is added to start the implementation of data extraction from MyAnimeList. The extraction process is functional and working as expected.

--- main.py
+++ main.py
@@ -2,9 +2,52 @@
 
 import requests
 import pandas as pd
+from colorama import init, Fore
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import json
 
-url = 'myanimelist.net/reviews.php?t=anime&filter_check=&filter_hide=&preliminary=on&spoiler=off&p=1'
+import requests
+from bs4 import BeautifulSoup
+from colorama import Fore
+
+def myanimelist(n_pages):
+    for n_page in range(n_pages):
+        # Set the URL of the page you want to scrape
+        url = f'https://myanimelist.net/reviews.php?t=anime&filter_check=&filter_hide=&preliminary=on&spoiler=off&p={n_page}'
+
+        # Send an HTTP GET request to the URL
+        response = requests.get(url)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the HTML content of the page using BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Find all review elements on the page
+            review_elements = soup.find_all(class_='review-element js-review-element')
+
+            # Iterate through each review element and extract relevant information
+            for review in review_elements:
+                # Extract the title of the anime from the review element
+                anime_title = review.find(class_='title ga-click').text.strip()
+
+                # Extract the numerical rating from the review element
+                rating_element = numerical_rating = review.find(class_='rating mt20 mb20 js-hidden')
+                numerical_rating = rating_element.find(class_='num').text.strip()
+
+                # Extract the text of the review from the review element
+                review_text = review.find(class_='text').text.strip()
+
+                # Print the extracted information
+                print(f'Title: {anime_title}')
+                print(f'Rating: {numerical_rating}')
+                print(f'Review: {Fore.WHITE}{review_text}{Fore.RESET}\n')
+        else:
+            print(f'Failed to retrieve the page. Status code: {response.status_code}')
+
+
+if __name__ == '__main__':
+    input_number = int(input('Enter the number of pages: '))
+    myanimelist(input_number)
